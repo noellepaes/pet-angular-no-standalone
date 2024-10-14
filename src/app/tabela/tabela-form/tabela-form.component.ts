a3ed4e730a7341715a46b5355077ab4d05dd0c98
“noellepaes” <noellepaes12345@gmail.com>
2024-10-14 19:01:01 -0300

    loiane eu sou tua fã aula 21

--- src/app/tabela/tabela-form/tabela-form.component.ts
+++ src/app/tabela/tabela-form/tabela-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
 import { TabelaService } from '../services/tabela.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -15,22 +16,32 @@ export class TabelaFormComponent {
   constructor(
     private formBuider: FormBuilder,
     private service: TabelaService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
   ){
     this.form = this.formBuider.group({
-      name:[null],
-      category: [null]
+      nome:[null],
+      categoria: [null]
     });
   }
 
   onSubmit(){
     this.service.save(this.form.value)
-    .subscribe(result => console.log(result), error => {
-      this.snackBar.open('erro ao salvar curso', '', {duration: 5000})
-    });
+    .subscribe(result => this.onSuccess(), error =>
+      this.onError());
   }
 
-  onCancel(){
 
+  onCancel(){
+    this.location.back();
+  }
+
+  private onSuccess(){
+    this.snackBar.open('Curso salvo com sucesso!', '', {duration: 5000});
+    this.onCancel();
+  }
+
+  private onError(){
+    this.snackBar.open('erro ao salvar curso', '', {duration: 5000});
   }
 }
