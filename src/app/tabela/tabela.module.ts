743b728e05ba059f97e789ca1c562e586da01e09
“noellepaes” <noellepaes12345@gmail.com>
2024-10-21 22:33:16 -0300

    movido pro container

--- src/app/tabela/tabela.module.ts
+++ src/app/tabela/tabela.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TabelaRoutingModule } from './tabela-routing.module';
-import { TabelaComponent } from './tabela/tabela.component';
+import { TabelaComponent } from './containers/tabela/tabela.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { TabelaFormComponent } from './tabela-form/tabela-form.component';
